bdba44cf74a4adb3fc133b1755996e01b9e3b7bf
sunben.96 <sunben.96@bytedance.com>
2024-09-04 21:43:27 +0800

    fix go 实验代码 2

--- 04-lang/01-go/02-std-cgo/main.go
+++ 04-lang/01-go/02-std-cgo/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os/user"
 )
 
 func main() {
-	u, err := user.Current()
+	u, err := user.Lookup("root")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello, World: %s\n", u.Username)
+	fmt.Printf("root uid: %s\n", u.Uid)
+	addrs, err := net.LookupHost("localhost")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("localhost addrs: %v\n", addrs)
 }
